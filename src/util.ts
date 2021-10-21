afa15c97ed58724e85ef9da99539c1829436faca
nurodev <me@nuro.dev>
2021-10-21 13:36:09 +0100

    Fixed usePubgHook recursion bug

--- src/util.ts
+++ src/util.ts
@@ -1,17 +1,27 @@
 import { useEffect, useState } from "react";
 
-export function usePubgHook<T, O>(props: O, hook: (props: O) => Promise<T>) {
+export function usePubgHook<T, O>(
+  props: O,
+  hook: (props: O) => Promise<T | null>
+) {
   const [data, setData] = useState<T | null>(null);
-  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    hook(props)
-      .then((player) => setData(player))
-      .catch((err) => setError(err));
-  }, [hook, props]);
+    if (!data && !error) {
+      setLoading(true);
+
+      hook(props)
+        .then((data) => setData(data))
+        .catch((err) => setError(err.message))
+        .finally(() => setLoading(false));
+    }
+  }, [props]);
 
   return {
     data,
+    loading,
     error,
   };
 }
